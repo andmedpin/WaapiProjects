ca6bef86a32291b4eadaf118dc9fc7fb65bf785c
Andres Medina <andres.pineda.medina@gmail.com>
2022-05-18 13:03:49 -0700

    Added Module verification code block

--- object_data.py
+++ object_data.py
@@ -7,16 +7,18 @@ wwise_core = "ak.wwise.core"
 
 
 args = {
-    #"from": {"path": ['\\Actor-Mixer Hierarchy\\Default Work Unit']},
-    "from" :  {"ofType" : ["Sound"]}
+    # "from": {"path": ['\\Actor-Mixer Hierarchy\\Default Work Unit']},
+    "from":  {"ofType": ["Sound"]}
+    # "from": {"id" : [object_creator.new_object_id]}
 }
 
 options = {
-    "return": ['id', 'name','type', "filePath"]
+    "return": ['id', 'name', 'type', 'filePath']
 }
 
-result = client.call(f"{wwise_core}.object.get", args, options = options)
+result = client.call(f"{wwise_core}.object.get", args, options=options)
 
-pprint(result)
+if __name__ == '__main__':
+    pprint(result)
 
 client.disconnect()
