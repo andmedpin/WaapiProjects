cebed58cfa4b42ce81d9faeaa1d21dd7bb3be083
Andres Medina <andres.pineda.medina@gmail.com>
2022-05-27 14:58:12 -0700

    Subscription script gets the appropriate info now. on_object_created function gets the id of the new object. on_object_selected function gets the type, id, and parent of the object.

--- subscription.py
+++ subscription.py
@@ -13,20 +13,49 @@ def on_name_changed(*args, **kwargs):
     new_name = kwargs.get("newName")
 
     print(f"Object {old_name} (of type {obj_type}) was renamed to {new_name}\n")
-    # pprint(kwargs.get("object", {}))
+    pprint(kwargs.get("object", {}))
     # client.disconnect()
 
 
 def on_object_created(*args, **kwargs):
-    new_object = kwargs.get("object", {})
-    object_id = kwargs.get("id")
-    pprint(f'New Object created. {object_id}')
+    object_selected = kwargs.get("object")
+    object_id = object_selected.get('id')
+    #object_type = object_selected.get('type')
+    #object_parent = object_selected.get('parent')
+
+    #pprint(kwargs)
+
+    pprint(f'New Object created ID: {object_id}')
+
+
+def on_object_selected(*args, **kwargs):
+    object_selected = kwargs.get("objects")[0]
+    object_id = object_selected.get('id')
+    object_type = object_selected.get('type')
+    object_parent = object_selected.get('parent')
+
+    # pprint(kwargs)
+
+    pprint(f'Object selected is of type: {object_type}, \n ID of the object: {object_id}, \n Parent: {object_parent}')
 
 
 if __name__ == '__main__':
-    # handler = client.subscribe("ak.wwise.core.object.nameChanged", on_name_changed, {"return": ["type","pluginName"]})
-    client.subscribe(
+    '''
+    handler_name = client.subscribe(
+        "ak.wwise.core.object.nameChanged", 
+        on_name_changed, 
+        {"return": ["type", 'parent']}
+    )
+    '''
+
+    handler_create = client.subscribe(
         "ak.wwise.core.object.created",
         on_object_created,
-        {"return": ["type", "id", "name"]}
+        {"return": ["id"]}
+    )
+
+    handler_select = client.subscribe(
+        "ak.wwise.ui.selectionChanged",
+        on_object_selected,
+        {"return": ["type", "id", "parent"]}
     )
